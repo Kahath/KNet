7ec87862a0bea37525aefc472dc44770e4782153
Kahath <slayer_Lac@hotmail.com>
2014-12-09 21:22:15 +0100

    Added packet handler example

---
+++ ServerFramework/Network/Packets/Handlers/ExampleHandler.cs
@@ -0,0 +1,38 @@
+﻿using ServerFramework.Constants.Attributes;
+using ServerFramework.Constants.Entities.Session;
+using ServerFramework.Constants.Misc;
+using ServerFramework.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerFramework.Network.Packets.Handlers
+{
+    public class ExampleHandler
+    {
+        [Opcode(0x0000, "Kahath", 1.0, OpcodeType.Test)]
+        public static void ExamplePacketHandler(UserToken packet)
+        {
+            Client pClient = Manager.SessionMgr.GetClientBySessionID(packet.SessionId);
+
+            //Read if packet has data
+            string testName = packet.Read<string>();
+            byte testData = packet.Read<byte>();
+
+            //Process data
+            Console.WriteLine(testName);
+
+            //Send back if need
+            //Get packet with opcode
+            UserToken token = pClient.PrepareSend(0x0001);
+
+            //Write data
+            token.Write<string>("Test string data");
+
+            //Send data
+            pClient.Send(token);
+        }
+    }
+}
